1bdb4e0ccdc43cf62acd8a0909083a3058132fc6
Владислав Гоголин <ddarean@gmail.com>
2021-10-31 23:50:35 +0300

    Hit quantity calc

--- Calc draft.py
+++ Calc draft.py
@@ -13,8 +13,7 @@ class Unit:
         
     @staticmethod
     def roll():
-        roll = random.randint(1, 6)
-        return roll
+        return random.randint(1, 6)
     
     def hit(self):
         roll = self.roll()
@@ -38,16 +37,39 @@ class Unit:
 def attack(attacker, defender):
     if attacker.hit():
         if attacker.wound(defender.t):
-            return defender.sv >= defender.roll() - attacker.ap
+            return defender.sv > defender.roll() - attacker.ap
     return False
 
 
-necron_warrior = Unit(ws=3, s=5, t=4, ap=2, sv=4, d=1, w=1)
-sm_intercessor = Unit(ws=3, s=4, t=4, ap=0, sv=3, d=1, w=2)
+def damage_chance(attacker, defender):
+    summ = 0
+    n = 10000
+    for i in range(0, n):
+        summ += attack(attacker, defender)
+    return summ / n
 
 
-summ = 0
-n = 10000
-for i in range(0, n):
-    summ += attack(necron_warrior, sm_intercessor)
-print(summ / n)
+def mean_hit_qty(n, p):
+    print(n * p - (1 - p))
+    print(n * p + p)
+
+
+necron_warrior_reaper = Unit(ws=3, s=5, t=4, ap=2, sv=4, d=1, w=1)
+sm_intercessor = Unit(ws=3, s=5, t=4, ap=2, sv=4, d=1, w=1)
+# chance = damage_chance(necron_warrior_reaper, sm_intercessor)
+# mean_hit_qty(20, chance)
+
+necron_warrior_flayer = Unit(ws=3, s=4, t=4, ap=1, sv=4, d=1, w=1)
+# chance = damage_chance(necron_warrior_flayer, sm_intercessor)
+# mean_hit_qty(20, chance)
+
+
+necron_immortal_gauss = Unit(ws=3, s=5, t=5, ap=2, sv=3, d=1, w=1)
+chance = damage_chance(necron_immortal_gauss, sm_intercessor)
+mean_hit_qty(10, chance)
+
+necron_immortal_tesla = Unit(ws=3, s=5, t=5, ap=0, sv=3, d=1, w=1)
+chance = damage_chance(necron_immortal_tesla, sm_intercessor)
+mean_hit_qty(13, chance)
+
+mean_hit_qty(10, 1/6)
